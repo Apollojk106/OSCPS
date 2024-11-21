f4273039168c2199e0477a3b936b72413a059845
Apollojk106 <jk586622@gmail.com>
2024-11-20 22:09:38 -0300

    emails decorados

--- resources/views/emails/reservation_status_changed.blade.php
+++ resources/views/emails/reservation_status_changed.blade.php
@@ -18,18 +18,59 @@
             <div class="text">Olá,</div>
             <div class="text">Esperamos que esteja bem.
                 <br> Estamos entrando em contato para informá-lo,
-                <br> sobre uma atualização importante em seu processo.</div>
+                <br> sobre uma atualização importante em seu processo.
+            </div>
             <br>
             @if($reservation)
-            <div class="text">A reserva de ID #{{ $reservation->id }} foi {{ $action }}.</div>
+            <div class="text">
+                A reserva da quadra poliesportiva,
+                <br>Com a data para o dia {{ \Carbon\Carbon::parse($reservation->date)->format('d/m/Y') }},
+                <br>No horário {{ \Carbon\Carbon::parse($reservation->date)->format('H:i:s') }} foi {{ $action }}.
+            </div>
             <br>
-            <div class="text">Status atual: {{ $reservation->status }}</div>
+            <div class="text">
+                Status atual:
+                <span class="font-bold 
+                    @if($reservation->status == 2)
+                        text-green-600  <!-- Para status 'aceito' -->
+                    @elseif($reservation->status == 3)
+                        text-red-600    <!-- Para status 'recusado' -->
+                    @endif">
+                    @if($reservation->status == 2)
+                    Aceito
+                    @elseif($reservation->status == 3)
+                    Recusado
+                    @endif
+                </span>
+            </div>
             @elseif($called)
-            <div class="text">O chamado #{{ $called->id }} teve seu status atualizado para {{ $status }}.</div>
+            <div class="text">
+                O seu chamado com ID#{{ $called->id }}
+                <br>teve seu status atualizado para <br> 
+                <span class="font-bold 
+                    @if($called->status == 2)
+                        text-yellow-500  <!-- Para status 'Em andamento' -->
+                    @elseif($called->status == 3)
+                        text-green-600   <!-- Para status 'Concluído' -->
+                    @endif">
+                    @if($called->status == 2)
+                    Em andamento
+                    @elseif($called->status == 3)
+                    Concluído
+                    @endif
+                </span>.
+                <br>Criado em: {{ \Carbon\Carbon::parse($called->created_at)->format('d/m/Y') }}.
+                <br>Local: {{ $called->environment }}.
+            </div>
+            <br>
+            <div class="text">
+                
+            </div>
             @endif
             <br>
-            <div class="text">Gostaríamos de agradecer pela sua colaboração e confiança. 
-                <br>Sua participação é fundamental para o sucesso do nosso sistema.</div>
+            <div class="text">Gostaríamos de agradecer pela sua colaboração e confiança.
+                <br>Sua participação é fundamental para a nossa instituição.
+            </div>
             <br>
             <div class="text">Atenciosamente, OSPCS</div>
             <div class="text">Equipe do Sistema</div>
